eb3bab1e001cd852b7f4b4628e176f7082842a6d
Nagul75 <nagulk697@gmail.com>
2025-03-24 23:31:02 +0530

    Implement Login Form

--- login-exam/src/components/Login.tsx
+++ login-exam/src/components/Login.tsx
@@ -1,12 +1,21 @@
+import Header from "./ui/Header"
+import Sidebar from "./ui/Sidebar"
+import LoginForm from "./ui/LoginForm"
 
+function Home() {
+  return (
+    <>
+    <div className="flex flex-col">
+      <Header title="Generic Website" loggedIn={false}></Header>
+      <div className="flex-1 flex mt-5 overflow-hidden">
+        <Sidebar></Sidebar>
+        <main className="flex-1 p-4 ml-68">
+          <LoginForm></LoginForm>
+        </main>
+      </div>
+    </div>
+    </>
+  )
+}
 
-
-function Login() {
-    return (
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Login Page
-      </h1>
-    )
-  }
-  
-export default Login
+export default Home
---
+++ login-exam/src/components/ui/LoginForm.tsx
@@ -0,0 +1,68 @@
+import { Button} from "./button";
+import { Input } from "./input";
+import { Card } from "./card";
+import { useState } from "react";
+
+const LoginForm = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+    })
+
+    const handleChange = (e:any) => {
+        const {name, value} = e.target;
+        setFormData({...formData, [name]: value})
+    }
+
+    const handleSubmit = (e:any) => {
+        e.preventDefault();
+        console.log('Form submitted', formData)
+    }
+
+    return(
+        <>
+            <div className="max-w-lg p-6">
+      <Card className="p-6 shadow-lg rounded-lg bg-stone-900 text-white border-stone-600">
+        <h2 className="text-3xl font-bold mb-4 text-center">Login</h2>
+
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="username" className="block text-sm font-medium text-white">Username</label>
+            <Input
+              id="username"
+              name="username"
+              type="text"
+              value={formData.username}
+              onChange={handleChange}
+              className="mt-2 p-2 w-full border rounded-lg text-white"
+              placeholder="Enter your username"
+            />
+
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-sm font-medium text-white">Password</label>
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              value={formData.password}
+              onChange={handleChange}
+              className="mt-2 p-2 w-full border rounded-lg"
+              placeholder="Enter your password"
+            />
+          </div>
+
+          <div className="mt-6 text-center">
+            <Button type="submit" className="w-full py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">
+              Register
+            </Button>
+          </div>
+        </form>
+      </Card>
+    </div>
+        </>
+    )
+}
+
+export default LoginForm
