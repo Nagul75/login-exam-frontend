fd361b9a0a72facb56c8bc47fcf302cd3e47ebf9
Nagul75 <nagulk697@gmail.com>
2025-03-26 12:18:08 +0530

    Style profile component

--- login-exam/src/components/Profile.tsx
+++ login-exam/src/components/Profile.tsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Card } from "./ui/card";
 import axios from "axios";
+import Header from "./ui/Header";
+import Sidebar from "./ui/Sidebar";
 
 interface UserData {
   id: number;
@@ -48,25 +52,48 @@ const Profile = () => {
   }
 
   return (
-    <div>
-      <h1>Welcome, {userData.fullname}</h1>
-      <p>
-        <strong>Username:</strong> {userData.username}
-      </p>
-      <p>
-        <strong>Email:</strong> {userData.email}
-      </p>
-      <p>
-        <strong>Account Created:</strong>{" "}
-        {new Date(userData.created).toLocaleDateString()}
-      </p>
-      <p>
-        <strong>Active:</strong> {userData.is_active ? "Yes" : "No"}
-      </p>
-      <p>
-        <strong>Staff:</strong> {userData.is_staff ? "Yes" : "No"}
-      </p>
-    </div>
+    <>
+      <div className="flex flex-col">
+        <Header title="Generic Website" loggedIn={true}></Header>
+        <div className="flex-1 flex mt-5 overflow-hidden">
+          <Sidebar></Sidebar>
+          <main className="flex-1 p-4 ml-64">
+            <Card className="p-6 shadow-lg rounded-lg bg-stone-900 text-white flex flex-col gap-1 w-2xl border-stone-700">
+              <h2 className="text-3xl font-bold mb-4 text-center">Profile</h2>
+              <div className="flex flex-row justify-start gap-4 mb-4">
+                <Avatar className="w-14 h-14">
+                  <AvatarFallback className="text-3xl text-black">
+                    {userData.username.slice(0, 2)}
+                  </AvatarFallback>
+                </Avatar>
+                <h1 className="text-white text-4xl">
+                  Welcome{" "}
+                  <span className="text-fuchsia-600">{userData.fullname}!</span>
+                </h1>
+              </div>
+              <div className="ml-18 border-l-stone-700 border-l-2 p-4">
+                <p>
+                  <strong>Username:</strong> {userData.username}
+                </p>
+                <p>
+                  <strong>Email:</strong> {userData.email}
+                </p>
+                <p>
+                  <strong>Account Created:</strong>{" "}
+                  {new Date(userData.created).toLocaleDateString()}
+                </p>
+                <p>
+                  <strong>Active:</strong> {userData.is_active ? "Yes" : "No"}
+                </p>
+                <p>
+                  <strong>Staff:</strong> {userData.is_staff ? "Yes" : "No"}
+                </p>
+              </div>
+            </Card>
+          </main>
+        </div>
+      </div>
+    </>
   );
 };
 
