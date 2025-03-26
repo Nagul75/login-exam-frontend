3fc9ecfb895e27b7320e7120fd2aecd1b775cc37
Nagul75 <nagulk697@gmail.com>
2025-03-26 22:05:33 +0530

    Set up settings page

--- login-exam/src/components/Settings.tsx
+++ login-exam/src/components/Settings.tsx
@@ -1,10 +1,20 @@
-function Settings() {
-    return (
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Settings page
-      </h1>
-    )
-  }
-  
-  export default Settings
-  
+import Header from "./ui/Header"
+import Sidebar from "./ui/Sidebar"
+
+function Home() {
+  return (
+    <>
+    <div className="flex flex-col">
+      <Header title="Generic Website" loggedIn={false}></Header>
+      <div className="flex-1 flex mt-5 overflow-hidden">
+        <Sidebar></Sidebar>
+        <main className="flex-1 p-4 ml-40 mt-18">
+          <h1 className="text-white">This is where <strong className="text-fuchsia-700">settings</strong> would be.</h1>
+        </main>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Home
