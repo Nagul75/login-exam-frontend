ab72891691b9cdce03a71e6e6d2ec99c8719768e
Nagul75 <nagulk697@gmail.com>
2025-03-26 18:20:08 +0530

    Get pokemon data from backend

--- login-exam/src/App.tsx
+++ login-exam/src/App.tsx
@@ -7,6 +7,9 @@ import axios from 'axios'
 function Home() {
   const navigate = useNavigate()
   const [loggedIn, setLoggedIn] = useState(false)
+  const [pokemonData, setPokemonData] = useState([])
+
+
   useEffect(()=> {
     axios.get('http://127.0.0.1:8000/check-auth/', {withCredentials: true})
       .then( res => {
@@ -22,6 +25,18 @@ function Home() {
         navigate("/login")
       })
   }, [])
+
+  useEffect(()=> {
+    axios.get('http://127.0.0.1:8000/')
+      .then((res) => {
+          setPokemonData(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
+
+
   return (
     <>
     <div className="flex flex-col">
@@ -29,7 +44,14 @@ function Home() {
       <div className="flex-1 flex mt-5 overflow-hidden">
         <Sidebar></Sidebar>
         <main className="flex-1 p-4 ml-40 mt-18">
-          <h1 className="text-white">Hello this is the main content</h1>
+          <h1 className="text-white">Hello, this is the main content</h1>
+          <h1 className="text-white">This is just dummy data from the server. This data can be anything: </h1>
+
+          <ul>
+            {pokemonData.map((pokemon, index) => (
+              <li className="text-white" key={index}>{pokemon.name}</li>
+            ))}
+          </ul>
         </main>
       </div>
     </div>
