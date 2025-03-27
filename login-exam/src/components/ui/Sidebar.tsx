abf4cf4b913e403a7cc6e96c258954435b644e1f
Nagul75 <nagulk697@gmail.com>
2025-03-27 11:48:52 +0530

    Change redirects for about and contacts

--- login-exam/src/components/ui/Sidebar.tsx
+++ login-exam/src/components/ui/Sidebar.tsx
@@ -7,8 +7,8 @@ const Sidebar = () => {
             <div className="w-38 text-white p-4 border-r-2 border-r-stone-800 fixed top-20 left-0 h-[calc(100vh-4rem)] z-40">
                 <div className="flex flex-col gap-4">
                 <Link to="/settings"><Button variant="link" className="text-white"><ChevronRight/>Settings</Button></Link>
-                <Link to="/settings"><Button variant="link" className="text-white"><ChevronRight/>About</Button></Link>
-                <Link to="/settings"><Button variant="link" className="text-white"><ChevronRight/>Contacts</Button></Link>
+                <Link to="/about"><Button variant="link" className="text-white"><ChevronRight/>About</Button></Link>
+                <Link to="/contacts"><Button variant="link" className="text-white"><ChevronRight/>Contacts</Button></Link>
                 </div>
             </div>
         </>
